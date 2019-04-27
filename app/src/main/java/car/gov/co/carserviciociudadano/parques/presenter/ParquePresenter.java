9139978b5f255a63790fd72d6b1a9dddbd98fc1a
Olger Rosero <olger.rosero@laguia.com.co>
2019-04-27 07:11:29 -0500

    cambio forma de generar colores de listado parques

--- app/src/main/java/car/gov/co/carserviciociudadano/parques/presenter/ParquePresenter.java
+++ app/src/main/java/car/gov/co/carserviciociudadano/parques/presenter/ParquePresenter.java
@@ -1,12 +1,12 @@
 package car.gov.co.carserviciociudadano.parques.presenter;
 import android.graphics.Color;
+import android.util.SparseArray;
 
-import com.anupcowkur.reservoir.Reservoir;
-import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import car.gov.co.carserviciociudadano.Utils.Enumerator;
-import car.gov.co.carserviciociudadano.Utils.Utils;
+import java.util.Map;
+
 import car.gov.co.carserviciociudadano.parques.dataaccess.Parques;
 import car.gov.co.carserviciociudadano.parques.interfaces.IParque;
 import car.gov.co.carserviciociudadano.parques.model.ErrorApi;
@@ -21,9 +21,21 @@ public class ParquePresenter {
     IViewParque iviewParque;
     String colores[] = {"#90962C","#BE0033","#9C8E70","#77B5BA","#E2A900","#005CA5"};
     String coloresFooter[] = {"#6F7724","#8E1731","#857555","#387775","#C5911A","#084A7A"};
+    int idsParques[] = {81,83,86,85,84,82};
+
+
+    SparseArray coloresMap = new SparseArray();
+    SparseArray coloresFooterMap = new SparseArray();
 
     public ParquePresenter(IViewParque iviewParque){
         this.iviewParque = iviewParque;
+        int i = 0;
+        for (int id : idsParques){
+            coloresMap.put(id, colores[i]);
+            coloresFooterMap.put(id, coloresFooter[i]);
+            i++;
+        }
+
     }
     public void list( )    {
         Parques parques = new Parques();
@@ -35,15 +47,27 @@ public class ParquePresenter {
         @Override
         public void onSuccess(List<Parque> lstParques) {
             int i = 0;
+            int indexJuanPablo = 0;
+            int indexGuatavita = 0;
             for(Parque item: lstParques){
-                if (i >= colores.length)
-                    i=0;
-
-                item.setColor(Color.parseColor(colores[i]));
-                item.setColorFooter(Color.parseColor(coloresFooter[i]));
                 item.setNombreParque(item.getNombreParque().replace("PARQUE",""));
+                if (i >= coloresMap.size()) {
+                    item.setColor(Color.parseColor(colores[0]));
+                    item.setColorFooter(Color.parseColor(coloresFooter[0]));
+                    break;
+                }
+
+                if (item.getIDParque() == 86) // ID Juan Pablo
+                    indexJuanPablo = i;
+                if (item.getIDParque() == 85) // ID Guatavita
+                    indexGuatavita = i;
+
+                item.setColor(Color.parseColor(coloresMap.get(item.getIDParque()).toString()));
+                item.setColorFooter(Color.parseColor(coloresFooterMap.get(item.getIDParque()).toString()));
                 i++;
             }
+            //cammbiar posicion entre Juan pablo y Guatavita
+          //  Collections.swap(lstParques, indexJuanPablo , indexGuatavita);
 
             iviewParque.onSuccess(lstParques);
         }
